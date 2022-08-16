e32fec053da4629c0779dbad913276c590dfa9db
sabbir <you@example.com>
2022-08-17 03:09:59 +0600

    math

--- math.cpp
+++ math.cpp
@@ -18,3 +18,22 @@ int main()
     }
     cout << " The sum of the above series is: " << sum << endl;
 }
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    double sum = 0, a;
+    int n, i;
+    cout << "\n\n Find the sum of the series 1 + 1/2^2 + 1/3^3 +.....+ 1/n^n:\n";
+    cout << "------------------------------------------------------------------\n";
+    cout << " Input the value for nth term: ";
+    cin >> n;
+    for(i =1; i <= n; ++i){
+        a = 1 / pow(i, i);
+        cout << "1/" << i << "^" << i << "=" << a << endl;
+        sum += a;
+    }
+    cout << " The sum of the above series is: " << sum << endl;
+}
